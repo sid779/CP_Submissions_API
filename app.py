5c3a293c44057c25aa5027ad0230ec2e8d5f5eae
sid779 <siddhartha779@gmail.com>
2020-06-27 03:41:44 +0530

    push14

--- app.py
+++ app.py
@@ -86,9 +86,9 @@ class Get_Submissions(Resource):
             url = 'https://www.stopstalk.com/user/profile/' + username
             chrome_options = webdriver.ChromeOptions()
             chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_PATH")
-            chrome_options.add_argument("--headless")
-            chrome_options.add_argument("--disable-dev-shm-usage")
-            chrome_options.add_argument("--no-sandbox")
+            #chrome_options.add_argument("--headless")
+            #chrome_options.add_argument("--disable-dev-shm-usage")
+            #chrome_options.add_argument("--no-sandbox")
             driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),chrome_options=chrome_options)
             driver.get(url)
             delay = 5  # seconds
